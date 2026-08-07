fbb19262a58a3a0f22eaeff57ac3dd1ecd5b2793
NahuelPVN <NR335448@fi365.ort.edu.uy>
2026-08-07 07:18:30 -0300

    feat: implement API for nodes and update frontend to fetch data

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,21 @@
+const nodes = [
+    { id: 0, name: "Internet", parent: null },
+    { id: 1, name: "HTTP/HTTPS", parent: 0 },
+    { id: 2, name: "Hosting", parent: 0 },
+    { id: 3, name: "DNS", parent: 0 },
+    { id: 4, name: "HTML", parent: null},
+    { id: 5, name: "CSS", parent: null},
+    { id: 6, name: "JavaScript", parent: null},
+    { id: 7, name: "Version control", parent: null},
+    { id: 8, name: "Git", parent: 7},
+    { id: 9, name: "GitHub/GitLab", parent: 7},
+    { id: 10, name: "Package Managers", parent: null},
+    { id: 11, name: "NPM", parent: 10},
+    { id: 12, name: "PNPM", parent: 10},
+    { id: 13, name: "VPS", parent: 2},
+    { id: 14, name: "Static", parent: 2}
+]
+
 const express = require("express");
 const app = express();
 
@@ -5,6 +23,10 @@ app.use(express.static("../frontend"));
 
 app.get("/", (req, res) => {
     res.send("Hello world");
+});
+
+app.get("/api/nodes", (req, res) => {
+    res.json(nodes);
 });
 
 app.listen(3000, () => {
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,22 +1,7 @@
-const nodes = [
-    { id: 0, name: "Internet", parent: null },
-    { id: 1, name: "HTTP/HTTPS", parent: 0 },
-    { id: 2, name: "Hosting", parent: 0 },
-    { id: 3, name: "DNS", parent: 0 },
-    { id: 4, name: "HTML", parent: null},
-    { id: 5, name: "CSS", parent: null},
-    { id: 6, name: "JavaScript", parent: null},
-    { id: 7, name: "Version control", parent: null},
-    { id: 8, name: "Git", parent: 7},
-    { id: 9, name: "GitHub/GitLab", parent: 7},
-    { id: 10, name: "Package Managers", parent: null},
-    { id: 11, name: "NPM", parent: 10},
-    { id: 12, name: "PNPM", parent: 10},
-    { id: 13, name: "VPS", parent: 2},
-    { id: 14, name: "Static", parent: 2}
-]
+async function main() {
+    const nodes = await (await fetch("/api/nodes")).json();
 
-function level(node) {
+    function level(node) {
     if (node.parent === null) {
         return 0
     }
@@ -24,26 +9,30 @@ function level(node) {
         const parent = nodes.find(n => n.id === node.parent);
         return(1 + level(parent));
     }
-}
-
-let tree = document.getElementById("tree-div");
-
-function createList(node) {
-    let sons = nodes.filter(n => n.parent === node.id);
-    if (sons.length === 0) {
-        return(`<li> ${node.name} </li>`);
-    } else {
-        let output = `<li> ${node.name} <ul>`;
-        for (const son of sons) {
-            output += createList(son);
-        }
-        output += `</ul> </li>`;
-        return(output);
     }
+
+    function createList(node) {
+        let sons = nodes.filter(n => n.parent === node.id);
+        if (sons.length === 0) {
+            return(`<li> ${node.name} </li>`);
+        } else {
+            let output = `<li> ${node.name} <ul>`;
+            for (const son of sons) {
+                output += createList(son);
+            }
+            output += `</ul> </li>`;
+            return(output);
+        }
+    }
+
+    let tree = document.getElementById("tree-div");
+
+    let roots = nodes.filter(n => n.parent === null);
+
+    for (const root of roots) {
+        tree.innerHTML += createList(root);
+    }
+
 }
 
-let roots = nodes.filter(n => n.parent === null);
-
-for (const root of roots) {
-    tree.innerHTML += createList(root);
-}
+main();
